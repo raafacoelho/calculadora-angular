83c52294871986d6aa347b186754cc180391c5ca
raafacoelho <ra_faellacoelho@hotmail.com>
2021-09-29 15:18:12 -0300

    Criação layout calculadora. Criação dos modulos e componentes da calculadora. Criação do serviço calculadora. Implementação do serviço e documentação. Criação de testes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CalculadoraModule } from './calculadora';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
